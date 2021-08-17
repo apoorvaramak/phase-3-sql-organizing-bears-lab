4419d038a058e8b355eec0fe23882946bbd14dda
Apoorva <apoorvar@comcast.net>
2021-08-17 10:15:58 -0500

    commit tree 91d4cdfc44bee6c12f45fdf2c09e7af29f70550d

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, sex, color) VALUES ("Mr.Chocolate", "M", "Brown");
+INSERT INTO bears (name, sex, color) VALUES ("Rowdy", "M", "Black");
+INSERT INTO bears (name, sex, color) VALUES ("Tabitha", "F", "Polar");
+INSERT INTO bears (name, sex, color) VALUES ("Sergeant Brown", "F", "Brown");
+INSERT INTO bears (name, sex, color) VALUES ("Melissa", "F", "Kodiak");
+INSERT INTO bears (name, sex, color) VALUES ("Grinch", "M", "Black");
+INSERT INTO bears (name, sex, color) VALUES ("Wendy", "F", "Moon");
+INSERT INTO bears (sex, color) VALUES ("F", "Kodiak");
